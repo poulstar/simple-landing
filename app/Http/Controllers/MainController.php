06ffe85d9b126d66edb96e476a6bd5774a88c47b
Mohammad Rasul Alizadeh <mohammadr.alizadeh@live.com>
2022-05-18 03:12:47 +0430

    first commit

---
+++ app/Http/Controllers/MainController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Register;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller as BaseController;
+
+class MainController extends BaseController
+{
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function register(Request $request) {
+        Register::create([
+            'name' => $request->post('name'),
+            'age' => $request->post('age'),
+            'tel' => $request->post('tel')
+        ]);
+        return back()->withSuccess('مشخصات شما ثبت شد. به زودی مشاوران ما با شما تماس خواهند گرفت');
+    }
+}
---
+++ resources/views/landing.blade.php
@@ -0,0 +1,161 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>پل استار - مشاوره رایگان</title>
+
+    <style>
+        @font-face {
+            font-family: "dana VF";
+            src: url("../../assets/fonts/dana/variable/dana2webGX.woff") format("woff-variations"),
+                url("../../assets/fonts/dana/variable/dana2webGX.woff") format("woff");
+            font-display: fallback;
+        }
+        body, html {
+            font-family: "dana VF";
+            line-height: 160%;
+            margin: 0;
+            padding: 0;
+            letter-spacing: -0.2px;
+            direction: rtl;
+        }
+        .cover {
+            width: 100%;
+            height: 100%;
+            object-fit: cover;
+            position: absolute;
+            top: 0;
+            left: 0;
+        }
+        .right {
+            right: 0;
+            width: 40%;
+            height: 100%;
+            display: flex;
+            justify-content: center;
+            align-content: center;
+        }
+        form {
+            margin: auto;
+            max-width: 400px;
+        }
+        .inner-from {
+            padding: 10%;
+            @if(Session::has('success'))
+            background-color: rgba(12, 202, 120, 0.9);
+            @else
+            background-color: rgba(255, 255, 255, 0.5);
+            @endif
+            border-radius: 10px;
+        }
+        .left, .right {
+            position: absolute;
+        }
+        .left {
+            left: 0;
+            width: 60%;
+            height: 100%;
+        }
+        label {
+            display: flex;
+            width: 100%;
+            overflow: hidden;
+            position: relative;
+            display: block;
+        }
+        .input {
+            width: 92%;
+            flex-grow: 1;
+            padding: 10px 3%;
+            font-family: "dana VF";
+            border: 2px solid lightgray;
+            outline: none;
+            border-radius: 8px;
+            margin-top: 15px;
+            display: block;
+            position: relative;
+        }
+        .send {
+            margin-top: 25px;
+            width: 100%;
+            color: white;
+            border: none !important;
+            text-align: center;
+            background-color: #263157;
+            cursor: pointer;
+        }
+        .form {
+            position: absolute;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+        }
+        .title {
+            font-size: 15px;
+            margin-bottom: 5vh;
+            text-align: justify;
+        }
+        .subtitle {
+            text-align: center;
+            font-weight: bold;
+            font-size: 14px;
+            margin-bottom: 25px;
+        }
+        .padding {
+            margin: 2%;
+            position: relative;
+            width: 94%;
+            height: 94%;
+        }
+        .alert {
+            font-weight: bold;
+            text-align: justify;
+            font-size: 14px;
+            color: white;
+        }
+    </style>
+</head>
+<body>
+    <img class="cover" src="{{ asset('2.jpg') }}" />
+    <div class="form">
+        <div class="padding">
+            <div class="right">
+                <form method="POST" action="{{ route('add-user') }}">
+                    @csrf
+                    <div class="title">
+                        خانواده پل استار از طریق مهارت آموزی و آموزش زبان برنامه نویسی به کودکان و نوجوانان، آنها را در مسیر دستیابی به توانمندی لازم برای موفقیت در زندگی و کار و لذت بردن از آن در قرن حاظر همراهی می کند.
+                    </div>
+                    <div class="inner-from">
+                        @if(Session::has('success'))
+                            <div class="alert alert-success">
+                                {{Session::get('success')}}
+                            </div>
+                        @else
+                            <div class="subtitle">
+                                با پر کردن فرم زیر شما در لیست مشاوره رایگان موسسه پل استار قرار خواهید گرفت:
+                            </div>
+                            <label>
+                                <input class="input" name="name" placeholder="نام و نام خانوادگی والد" required/>
+                            </label>
+                            <label>
+                                <input class="input" type="number" name="age" placeholder="سن فرزند" required/>
+                            </label>
+                            <label>
+                                <input class="input" type="tel" name="tel" placeholder="شماره موبایل" required/>
+                            </label>
+                            <input class="" type="number" name="hours" hidden/>
+                            <button type="submit" class="input send">ارسال</button>
+                        @endif
+                    </div>
+                </form>
+            </div>
+            <div class="left">
+
+            </div>
+        </div>
+    </div>
+</body>
+</html>
